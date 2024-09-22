3fc2da4357ac26a6d5133d519e3e756a36a54d6d
A. Developers <ahmarjabbar7@gmail.com>
2024-09-23 02:29:36 +0500

    nw

--- BE/routes/coinRoute.js
+++ BE/routes/coinRoute.js
@@ -1,6 +1,6 @@
 let express = require("express");
 
-const { authorizedRoles, isAuthorizedUser } = require("../middlewares/auth");
+const { authorizedRoles } = require("../middlewares/auth");
 const {
   addCoins,
   getCoins,
@@ -22,30 +22,30 @@ const {
 
 let router = express.Router();
 
-router.route("/addCoins/:id").patch(isAuthorizedUser, addCoins);
-router.route("/updateCoinAddress/:id").patch(isAuthorizedUser, updateCoinAddress);
-router.route("/getCoins/:id").get(isAuthorizedUser, getCoins);
-router.route("/getUserCoin/:id").get(isAuthorizedUser, getUserCoin);
+router.route("/addCoins/:id").patch(addCoins);
+router.route("/updateCoinAddress/:id").patch(updateCoinAddress);
+router.route("/getCoins/:id").get(getCoins);
+router.route("/getUserCoin/:id").get(getUserCoin);
 
-router.route("/getCoinsUser/:id").get(isAuthorizedUser, getCoinsUser);
+router.route("/getCoinsUser/:id").get(getCoinsUser);
 router
   .route("/deleteTransaction/:userId/:transactionId")
   .get(deleteTransaction);
 router
   .route("/deleteUserStocksApi/:id/:coindId")
   .delete(deleteUserStocksApi);
-router.route("/createTransaction/:id").patch(isAuthorizedUser, createTransaction);
-router.route("/createUserStocks/:id").post(isAuthorizedUser, createUserStocks);
-router.route("/createUserTransaction/:id").patch(isAuthorizedUser, createUserTransaction);
+router.route("/createTransaction/:id").patch(createTransaction);
+router.route("/createUserStocks/:id").post(createUserStocks);
+router.route("/createUserTransaction/:id").patch(createUserTransaction);
 router
   .route("/createUserTransactionWithdrawSwap/:id")
-  .patch(isAuthorizedUser, createUserTransactionWithdrawSwap);
+  .patch(createUserTransactionWithdrawSwap);
 router
   .route("/createUserTransactionDepositSwap/:id")
-  .patch(isAuthorizedUser, createUserTransactionDepositSwap);
-router.route("/updateTransaction/:id").patch(isAuthorizedUser, updateTransaction);
-router.route("/getTransactions").get(isAuthorizedUser, getTransactions);
-router.route("/getEachUser/:id").get(isAuthorizedUser, getEachUser);
-router.route("/deleteEachUser/:id").delete(isAuthorizedUser, deleteEachUser);
+  .patch(createUserTransactionDepositSwap);
+router.route("/updateTransaction/:id").patch(updateTransaction);
+router.route("/getTransactions").get(getTransactions);
+router.route("/getEachUser/:id").get(getEachUser);
+router.route("/deleteEachUser/:id").delete(deleteEachUser);
 
 module.exports = router;
