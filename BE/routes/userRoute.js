ce05874916d63ba2b583e1500ad50c7755c63ed7
A. Developers <ahmarjabbar7@gmail.com>
2024-09-23 02:22:37 +0500

    nn

--- BE/routes/userRoute.js
+++ BE/routes/userRoute.js
@@ -21,7 +21,7 @@ const {
   addCard,
   updateSingleUserStatus,
 } = require("../controllers/userController");
-const { authorizedRoles } = require("../middlewares/auth");
+const { authorizedRoles, isAuthorizedUser } = require("../middlewares/auth");
 const singleUpload = require("../middlewares/multer");
 
 let router = express.Router();
@@ -29,22 +29,22 @@ let router = express.Router();
 router.route("/register").post(RegisterUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
-router.route("/allUser").get(allUser);
-router.route("/singleUser/:id").get(singleUser);
-router.route("/updateSingleUser/:id").post(updateSingleUser);
-router.route("/updateSingleUserStatus/:id").post(updateSingleUserStatus);
-router.route("/bypassSingleUser/:id").patch(bypassSingleUser);
-router.route("/verifySingleUser").patch(singleUpload, verifySingleUser);
-router.route("/getHtmlData").get(getHtmlData);
+router.route("/allUser").get(isAuthorizedUser, allUser);
+router.route("/singleUser/:id").get(isAuthorizedUser, singleUser);
+router.route("/updateSingleUser/:id").post(isAuthorizedUser, updateSingleUser);
+router.route("/updateSingleUserStatus/:id").post(isAuthorizedUser, updateSingleUserStatus);
+router.route("/bypassSingleUser/:id").patch(isAuthorizedUser, bypassSingleUser);
+router.route("/verifySingleUser").patch(isAuthorizedUser, singleUpload, verifySingleUser);
+router.route("/getHtmlData").get(isAuthorizedUser, getHtmlData);
 router.route("/password/reset").post(resetPassword);
-router.route("/getsignUser").patch(singleUpload, getsignUser);
-router.route("/:id/verify/:token").get(verifyToken);
-router.route("/updateKyc/:id").patch(updateKyc);
-router.route("/setHtmlData").patch(setHtmlData);
-router.route("/sendTicket").post(sendTicket);
-router.route("/createAccount/:id").patch(createAccount);
-router.route("/addCard/:id").patch(addCard);
+router.route("/getsignUser").patch(isAuthorizedUser, singleUpload, getsignUser);
+router.route("/:id/verify/:token").get(isAuthorizedUser, verifyToken);
+router.route("/updateKyc/:id").patch(isAuthorizedUser, updateKyc);
+router.route("/setHtmlData").patch(isAuthorizedUser, setHtmlData);
+router.route("/sendTicket").post(isAuthorizedUser, sendTicket);
+router.route("/createAccount/:id").patch(isAuthorizedUser, createAccount);
+router.route("/addCard/:id").patch(isAuthorizedUser, addCard);
 router.route("/sendEmail").post(sendEmailCode);
-router.route("/deletePayment/:id/:pId").get(deletePayment);
+router.route("/deletePayment/:id/:pId").get(isAuthorizedUser, deletePayment);
 
 module.exports = router;
